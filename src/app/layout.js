793662aa97e331dcd5153c603c7166e8444bd8af
TechTrio Automation <techtrioautomation@gmail.com>
2025-04-16 15:33:57 +0530

    Loader and SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,40 @@
+// app/layout.js
 import { ThemeProvider } from "next-themes";
-import Navbar from "./../components/Navbar";
 import "./globals.css";
-
-import Footer from "./../components/Footer";
-
+import ClientLayout from "../components/ClientLayout"; // no dynamic import here
 
 export const metadata = {
-  title: "Your App",
-  description: "Next.js App with Dark Mode",
+  title: "TechTrio - Smart Parking, Invoicing & Tracking SaaS",
+  description:
+    "TechTrio offers SaaS solutions including Smart Parking Systems, Invoice Management, and Vehicle Tracking. Revolutionize urban mobility and business automation with our innovative technologies.",
+  keywords:
+    "TechTrio, SaaS, Smart Parking, Parking Solutions, Parking Management, Invoice System, Invoice Management Software, Vehicle Tracking, GPS Tracking, Fleet Management, Parking App, Automation, Tech Company, Smart City Solutions",
+  openGraph: {
+    title: "TechTrio - Smart Parking Solutions & Automation SaaS",
+    description:
+      "TechTrio is revolutionizing urban parking and automation with Smart Parking Systems, Invoice Management, and Vehicle Tracking solutions. Explore our cutting-edge SaaS technology today.",
+    url: "https://www.techtrio.net",
+    type: "website",
+    siteName: "TechTrio",
+  },
+  authors: [{ name: "TechTrio Automation", url: "https://www.techtrio.net" }],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: "/assets/images/logo/favicon.ico",
+  },
+  metadataBase: new URL("https://www.techtrio.net"),
+  viewport: "width=device-width, initial-scale=1.0",
 };
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-colors">
-       
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <Navbar />
-          {children}
-
-          <Footer/>
-
+          <ClientLayout>{children}</ClientLayout>
         </ThemeProvider>
-
       </body>
     </html>
   );
